4cd6b4a20e57fd105a3dff1127b463699b45c020
Fiona Sanggang <fsanggang@gmail.com>
2020-10-31 14:48:18 +1300

    Add Post.Snippet

--- Data/Post.cs
+++ Data/Post.cs
@@ -4,9 +4,19 @@ namespace Golb.Data
 {
     public class Post
     {
+        private const int PREVIEW_LENGTH = 512;
+
         public int Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
         public DateTime PostedAt { get; set; }
+
+        public string Snippet
+        {
+            get
+            {
+                return (this.Body.Length <= PREVIEW_LENGTH) ? this.Body : this.Body.Substring(0, PREVIEW_LENGTH) + "...";
+            }
+        }
     }
 }
